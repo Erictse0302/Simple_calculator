8f577d8af5e31b928df32bb5d505aff131be784c
Erictse0302 <113747030+Erictse0302@users.noreply.github.com>
2023-08-25 17:31:20 +0800

    Add files via upload

---
+++ Java_Cal/cal.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class cal{
+
+    public static void main(String a [])
+    {
+        System.out.println("This is a simple calculator \n\n\n add Please input a \n subtraction Please input b \n multiplication Please input c \n division Please input d ");
+        String opeator; 
+        int input1 , input2;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("\n Choose Your Opeator");
+        opeator = sc.nextLine();
+        System.out.println("Enter the first Number");
+        input1 = sc.nextInt();
+        System.out.println("Enter the Second Number");
+        input2 = sc.nextInt();
+
+        int result = 0; 
+
+        
+
+        switch(opeator)
+        {
+          case "a" -> result = input1 + input2;
+          case "b" -> result = input1 - input2;
+          case "c"-> result = input1 * input2;
+          case "d" -> result = input1 / input2;
+          default-> System.out.println("Wrong Case");
+        };
+        
+        if (result != 0)
+            System.out.println("\n" + result);
+      
+        sc.close();
+    }
+}
