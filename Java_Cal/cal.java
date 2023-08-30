62b3e711ceb04390640eb174906317bbd172996f
Erictse0302 <113747030+Erictse0302@users.noreply.github.com>
2023-08-30 14:14:49 +0800

    Update cal.java
    To make the cal loop

--- Java_Cal/cal.java
+++ Java_Cal/cal.java
@@ -1,36 +1,55 @@
 import java.util.Scanner;
 
-public class cal{
+public class cal 
+{
 
     public static void main(String a [])
-    {
-        System.out.println("This is a simple calculator \n\n\n add Please input a \n subtraction Please input b \n multiplication Please input c \n division Please input d ");
-        String opeator; 
+    {   
         int input1 , input2;
+        int op;
+        int result = 0;
+        while(true){
+        System.out.println("This is a simple calculator \n\n\n add Please input 1 \n subtraction Please input 2 \n multiplication Please input 3 \n division Please input 4 \n Exit Please input 5 ");
         Scanner sc = new Scanner(System.in);
         
         System.out.println("\n Choose Your Opeator");
-        opeator = sc.nextLine();
-        System.out.println("Enter the first Number");
-        input1 = sc.nextInt();
-        System.out.println("Enter the Second Number");
-        input2 = sc.nextInt();
-
-        int result = 0; 
-
-        
-
-        switch(opeator)
-        {
-          case "a" -> result = input1 + input2;
-          case "b" -> result = input1 - input2;
-          case "c"-> result = input1 * input2;
-          case "d" -> result = input1 / input2;
-          default-> System.out.println("Wrong Case");
-        };
-        
-        System.out.println("The Result is" + result)
+        op = sc.nextInt();
       
-        sc.close();
+
+        switch(op)
+        {
+          case 1 -> { System.out.println("Enter the first Number");
+                      input1 = sc.nextInt();
+                      System.out.println("Enter the Second Number");
+                      input2 = sc.nextInt();
+            
+                      result = input1 - input2;}
+          case 2 -> { System.out.println("Enter the first Number");
+                      input1 = sc.nextInt();
+                      System.out.println("Enter the Second Number");
+                      input2 = sc.nextInt();
+            
+                      result = input1 + input2;}
+          case 3-> { System.out.println("Enter the first Number");
+                      input1 = sc.nextInt();
+                      System.out.println("Enter the Second Number");
+                      input2 = sc.nextInt();
+            
+                      result = input1* input2;}
+          case 4 -> { System.out.println("Enter the first Number");
+                      input1 = sc.nextInt();
+                      System.out.println("Enter the Second Number");
+                      input2 = sc.nextInt();
+            
+                      result = input1 / input2;}
+          case 5 -> {System.out.println(" Thank You for use  \n" ); 
+                     System.exit(0);}
+          default-> System.out.println("Wrong Case");
+        }
+       
+
+        System.out.println("\n" + result);}
+        
+      
     }
 }
